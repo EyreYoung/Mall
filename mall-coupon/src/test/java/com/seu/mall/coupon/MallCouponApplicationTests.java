c4fd981ed66455c306c722563efb518bfaca7050
Eyre Young <yudongyang1996@gmail.com>
2020-04-30 16:15:01 +0800

    商城项目初始结构创建

---
+++ mall-coupon/src/test/java/com/seu/mall/coupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seu.mall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
